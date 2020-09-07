aaf07ef25b1965f296d67c094a5089a172021947
Aman Khairkar <aman.khairkar555@gmail.com>
2020-09-07 17:53:37 +0530

    Added UI for main menu
    Added new UI for main menu under Menu Scene
    added 2 new scene for basic and Trap/Parkour arena
    Added 2 new scripts for UI effects and scene loading
    added 2 new prefabs of UI

---
+++ ProjectData/UnityProject/ArenaConquerer/Assets/Scripts/Common/UI/ButtonLogics.cs
@@ -0,0 +1,45 @@
+﻿using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonLogics : MonoBehaviour
+{
+    [SerializeField] private Button increment;
+    [SerializeField] private Button decrement;
+    [SerializeField] private TMP_InputField inputField;
+    [SerializeField] private int MaxValue;
+
+    private void Start()
+    {
+        increment.onClick.AddListener(() =>
+        {
+            IncrementValue();
+        });
+
+        decrement.onClick.AddListener(() =>
+        {
+            DecrementValue();
+        });
+    }
+
+    /// <summary>
+    /// Increment
+    /// </summary>
+    /// <param name="MaxValue">Use this parameter to set max value for that input field</param>
+    public void IncrementValue()
+    {
+        int value = int.Parse(inputField.text);
+        value += 1;
+        inputField.text = value.ToString();
+    }
+
+    public void DecrementValue()
+    {
+        int value = int.Parse(inputField.text);
+        if (value > 0)
+        {
+            value -= 1;
+            inputField.text = value.ToString();
+        }
+    }
+}
